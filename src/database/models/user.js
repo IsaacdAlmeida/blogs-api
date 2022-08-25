ec368fa2d110b77904769d90ca1abc96b1381496
Isaac Almeida <isaacalmeidafilho@gmail.com>
2022-08-25 17:22:45 -0300

    feat(sequelize): create user model

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,23 +1,25 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  User.init({
-    id: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'User',
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },{
+    tableName: 'User',
   });
+
+  User.associate = (models) => {
+    User.belongsTo(models.blogPost, {
+      foreignKey: 'userId',
+      as: 'blogpost',
+    });
+  };
+
   return User;
 };
