22a5ba6d5f9c3d94c33a3408c7100beb844c46c6
Isaac Almeida <isaacalmeidafilho@gmail.com>
2022-08-25 18:01:39 -0300

    feat(sequelize): create postCategory model

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      foreignKey: true,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      foreignKey: true,
+    },
+  },{
+    tableName: 'PostCategories',
+  });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      through: PostCategory,
+      as: 'categories',
+      foreignKey: 'postId',
+      otherKey: 'categoryId'
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      through: PostCategory,
+      as: 'blogPosts',
+      foreignKey: 'categoryId',
+      otherKey: 'postId'
+    });
+  };
+
+  return PostCategory;
+};
