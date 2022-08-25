10bb8b83aaa26f49126517b43dc3410cd50a9584
Isaac Almeida <isaacalmeidafilho@gmail.com>
2022-08-25 17:28:37 -0300

    feat(sequelize): create category model

---
+++ src/database/models/category.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: DataTypes.STRING,
+  },{
+    tableName: 'Categories',
+  });
+
+  return Category;
+};
