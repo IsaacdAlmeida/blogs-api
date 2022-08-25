ef17e3d2086aa93b3b0225d8a6ce642a42df34fc
Isaac Almeida <isaacalmeidafilho@gmail.com>
2022-08-25 17:02:37 -0300

    feat: create all migrations
    finish requirement 01

---
+++ src/database/migrations/20220825195356-create-post-categories.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostCategories', {
+      postId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        foreignKey: true,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'BlogPosts',
+          key: 'id'
+        }
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        foreignKey: true,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Categories',
+          key: 'id'
+        }
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostCategories');
+  }
+};
