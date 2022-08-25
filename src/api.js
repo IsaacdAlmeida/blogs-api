32044921da02bb57970edc696d99c8b3479201e7
Isaac Almeida <isaacalmeidafilho@gmail.com>
2022-08-25 14:39:41 -0300

    initial commit

--- src/api.js
+++ src/api.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 
-// ...
+// ... initial commit
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
